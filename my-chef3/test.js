da3aac72d7987edf32f790d5598deaa67f45a035
Cong Wang <cngwang6@gmail.com>
2017-04-15 15:56:19 -0400

    Nutrient report work in progress. Leaving as is due to time constraints.

--- my-chef3/test.js
+++ my-chef3/test.js
@@ -40,7 +40,7 @@ var options = {
   "method": "GET",
   "hostname": "api.nal.usda.gov",
   "port": null,
-  "path": "/ndb/search/?format=json&sort=n&max=25&offset=0&fg=fruits%20and%20fruit%20juices&api_key=Q2W8cDINhmomMkw2Qv91Vq3laaACY2NB8J54WsdI",
+  "path": "/ndb/search/?format=json&sort=n&max=25&offset=0&q=raw&fg=fruits%20and%20fruit%20juices&api_key=Q2W8cDINhmomMkw2Qv91Vq3laaACY2NB8J54WsdI",
   "headers": {
     "authorization": "Bearer=eyJpZCI6IjAwMDAwMDAwMDAwMDAwMDAwMDAwMDAwMSJ9",
     "cache-control": "no-cache",
@@ -65,42 +65,41 @@ var req = http.request(options, function(res) {
     for (var x = 0; x < length; x++) {
       fruit.push(jsonObj.list.item[x].name);
       fruitDBN.push(jsonObj.list.item[x].ndbno);
-      console.log(fruitDBN[x]);
+      console.log(fruit);
     }
   // console.log(body.toString());
   });
 });
+  // var options = {
+  //   "method": "GET",
+  //   "hostname": "api.nal.usda.gov",
+  //   "port": null,
+  //   "path": "/ndb/nutrients/?format=json&api_key=Q2W8cDINhmomMkw2Qv91Vq3laaACY2NB8J54WsdI&nutrients=205&nutrients=204&nutrients=208&nutrients=269&ndbno=01009",
+  //   "headers": {
+  //     "cache-control": "no-cache",
+  //     "postman-token": "f4a4d3f3-2966-9ef7-cf2c-11f177947d71"
+  //   }
+  // };
+  // console.log('pulling info');
+  // var req = http.request(options, function(res) {
+  //   var chunks = [];
+  //   res.on("data", function(chunk) {
+  //     chunks.push(chunk);
+  //   // fruit.push(chunk);
+  //   // console.log(fruit);
+  //   });
 
+  //   res.on("end", function() {
+  //     var body = Buffer.concat(chunks);
+  //     var jsonObj = JSON.parse(body.toString());
+  //     console.log(jsonObj);
+  //     fruitInfo.push(jsonObj.report.foods[0].nutrients[0].unit+" "+jsonObj.report.foods[0].nutrients[0].value);
+  //     console.log(fruitInfo[0]);
+  //     // fruitInfo[x] = jsonObj;
+  //   // console.log(body.toString());
+  //   });
+  // });
 req.end();
 //fruitDBN.length
-for (var x = 0; x < 5; x++){
-  var options = {
-    "method": "GET",
-    "hostname": "api.nal.usda.gov",
-    "port": null,
-    "path": "/ndb/nutrients/?format=json&api_key=Q2W8cDINhmomMkw2Qv91Vq3laaACY2NB8J54WsdI&nutrients=205&nutrients=204&nutrients=208&nutrients=269&ndbno="+fruitDBN[x],
-    "headers": {
-      "cache-control": "no-cache",
-      "postman-token": "f4a4d3f3-2966-9ef7-cf2c-11f177947d71"
-    }
-  };
-
-  var req = http.request(options, function(res) {
-    var chunks = [];
-    res.on("data", function(chunk) {
-      chunks.push(chunk);
-    // fruit.push(chunk);
-    // console.log(fruit);
-    });
-
-    res.on("end", function() {
-      var body = Buffer.concat(chunks);
-      var jsonObj = JSON.parse(body.toString());
-      console.log(jsonObj);
-      fruitInfo.push(jsonObj.report.foods.nutrients[0].unit+" "+jsonObj.report.foods.nutrients[0].value);
-      console.log(fruitInfo[x]);
-      // fruitInfo[x] = jsonObj;
-    // console.log(body.toString());
-    });
-  });
-}
+// for (var x = 0; x < 5; x++){
+// }
